f8e4d9b07dc39854ae658bb78b614b59df5666ce
Jonas Forsberg <jonasf@Jonass-MacBook-Pro.local>
2019-06-10 09:46:24 +0200

    startscreen component

---
+++ frontend/api_u2/src/StartScreen/StartScreenComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class StartScreenComponent extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
