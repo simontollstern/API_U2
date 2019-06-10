fbf45f2fcd3c09bee08dc412bff3766f734766dc
simontollstern <simoon909@hotmail.com>
2019-06-10 14:07:09 +0200

    Added to beverage list

--- frontend/api_u2/src/BeverageList/BeverageListComponent.js
+++ frontend/api_u2/src/BeverageList/BeverageListComponent.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react'
 import style from './BeverageListComponent.module.css';
 
 export default class BeverageListComponent extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      beverages: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3001/beverages')
+      .then(res => res.json())
+      .then(beverages => this.setState({beverages: beverages}));
+  }
+
   render() {
     return (
       <div>
-        <p>Beverage List</p>
+        <h2>Nån sökfunktion här</h2>
+        <ul>
+          {this.state.beverages.map(beverage => {
+            return <li key={beverage.id}><a href="">{beverage.nameBold}</a></li>
+          })}
+        </ul>
       </div>
     )
   }
