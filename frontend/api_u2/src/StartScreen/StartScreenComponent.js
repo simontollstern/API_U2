a1d269c4420f0da059f3d3aa2937517c836140e5
Jonas Forsberg <jonasf@Jonass-MacBook-Pro.local>
2019-06-10 11:23:06 +0200

    some frontend

--- frontend/api_u2/src/StartScreen/StartScreenComponent.js
+++ frontend/api_u2/src/StartScreen/StartScreenComponent.js
@@ -4,9 +4,25 @@ import style from './StartScreenComponent.module.css';
 export default class StartScreenComponent extends Component {
   render() {
     return (
-      <div className={style.sidebarWrapper}>
-        
-      </div>
+      <header>
+        <div className={style.sidebarWrapper}>
+          <h2>Hello</h2>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">
+                Hem
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                Recensioner
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     )
   }
 }
