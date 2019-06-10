34d223fa2d9b45f0c5c994aa823ba0cecf66e71d
simontollstern <simoon909@hotmail.com>
2019-06-10 13:25:27 +0200

    Added some components

---
+++ frontend/api_u2/src/ReviewList/ReviewListComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import style from './ReviewListComponent.module.css';
+
+export default class ReviewListComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>Review List</p>
+      </div>
+    )
+  }
+}
